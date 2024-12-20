1a7abfb9fedd550f7effcc70c22e62191b70ee02
Jan Żukowski <janzukowski@proton.me>
2024-12-20 21:38:18 +0100

    initial commit

---
+++ ProblemKomiwojazera/Program.cs
@@ -0,0 +1,11 @@
+﻿// Zapoznaj się z problemem św. Mikołaja (zwanym również problemem komiwojażera)
+
+// Napisz program którzy przyjmie listę 50 współrzędnych gdzie -100 < x <100, -100 < y <100 jako plik lub tekst wklejony do kontrolki.
+
+// Dokonaj deserializacji JSONa i oblicz w dowolny sposób jakąkolwiek trasę (nie musi być najkrótsza)
+
+// Wyświetl na ekranie:
+
+// Całkowitą długość trasy
+// Kolejność odwiedzonych punktów
+// (opcjonalnie) czas wykonania obliczeń
